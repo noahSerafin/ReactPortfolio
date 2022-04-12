e005c54397e24f60c828cfc9f7071dd3600188f1
noahSerafin <noah.serafin04@gmail.com>
2022-04-12 17:20:51 +0100

    app-index

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Contact from "./pages/Contact/Contact";
+import Projects from "./pages/Projects/Projects";
+import Graphics from "./pages/Graphics/Graphics";
+import Home from "./pages/Home/Home";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/projects" element={<Projects />} />
+      <Route path="/graphics" element={<Graphics />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/" element={<Home />} />
+    </Routes>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -3,13 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router } from "react-router-dom";
+import NavBar from './components/Navbar/Navbar';
+import ThreeDBackground from './components/ThreeDBackground/ThreeDBackground';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router >
+      <ThreeDBackground />
+      <NavBar />      
+      <App />      
+    </Router>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
